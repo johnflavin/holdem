45fcb28b72cf7afd2f5951e7a277de44a9f385df
Flavin <flavinj@gmail.com>
2012-02-14 13:19:28 -0600

    to decrease my confusion, changed name of "hand" data member to "cards"

--- pokerhandtype.py
+++ pokerhandtype.py
@@ -1,8 +1,8 @@
 class PokerHandType:
 	rank = -1
 
-	def __init__(self,poker_hand):
-		self.hand = poker_hand
+	def __init__(self,card_list):
+		self.cards = card_list
 		self.string = 'Hand Type'
 
 	def __lt__(self, other):
@@ -37,104 +37,101 @@ class PokerHandType:
 class HighCard(PokerHandType):
 	rank = 0
 
-	def __init__(self,poker_hand):
-		self.hand = poker_hand
-		self.string = '{} High'.format(self.hand[-1].name)
+	def __init__(self,card_list):
+		self.cards = card_list
+		self.string = '{} High'.format(self.cards[-1].name)
 
 	def __lt__(self, other):
 		if self.rank == other.rank:
-			return self.hand < other.hand
+			return self.cards < other.cards
 		return self.rank < other.rank
 
 	def __le__(self, other):
 		if self.rank == other.rank:
-			return self.hand <= other.hand
+			return self.cards <= other.cards
 		return self.rank <= other.rank
 
 	def __eq__(self, other):
 		if self.rank == other.rank:
-			return self.hand == other.hand
+			return self.cards == other.cards
 
 	def __ne__(self, other):
 		if self.rank == other.rank:
-			return self.hand != other.hand
+			return self.cards != other.cards
 		return self.rank != other.rank
 
 	def __gt__(self, other):
 		if self.rank == other.rank:
-			return self.hand > other.hand
+			return self.cards > other.cards
 		return self.rank > other.rank
 
 	def __ge__(self, other):
 		if self.rank == other.rank:
-			return self.hand >= other.hand
+			return self.cards >= other.cards
 		return self.rank >= other.rank
-
-	def high_card(self):
-		return self.hand[-1]
 
 class Pair(PokerHandType):
 	rank = 1
 
-	def __init__(self,poker_hand):
-		self.hand = poker_hand
-		self.pair = self.hand[-1].value
-		self.string = 'Pair of {}s'.format(self.hand[-1].name)
+	def __init__(self,card_list):
+		self.cards = card_list
+		self.pair = self.cards[-1].value
+		self.string = 'Pair of {}s'.format(self.cards[-1].name)
 
 	def __lt__(self, other):
 		if self.rank == other.rank:
 			if self.pair == other.pair:
-				return self.hand < other.hand
+				return self.cards < other.cards
 			return self.pair < other.pair
 		return self.rank < other.rank
 
 	def __le__(self, other):
 		if self.rank == other.rank:
 			if self.pair == other.pair:
-				return self.hand <= other.hand
+				return self.cards <= other.cards
 			return self.pair < other.pair
 		return self.rank <= other.rank
 
 	def __eq__(self, other):
 		if self.rank == other.rank:
 			if self.pair == other.pair:
-				return self.hand == other.hand
+				return self.cards == other.cards
 		return False
 
 	def __ne__(self, other):
 		if self.rank == other.rank:
 			if self.pair == other.pair:
-				return self.hand != other.hand
+				return self.cards != other.cards
 		return True
 
 	def __gt__(self, other):
 		if self.rank == other.rank:
 			if self.pair == other.pair:
-				return self.hand > other.hand
+				return self.cards > other.cards
 			return self.pair > other.pair
 		return self.rank > other.rank
 
 	def __ge__(self, other):
 		if self.rank == other.rank:
 			if self.pair == other.pair:
-				return self.hand >= other.hand
+				return self.cards >= other.cards
 			return self.pair > other.pair
 		return self.rank >= other.rank
 
 class TwoPair(PokerHandType):
 	rank = 2
 
-	def __init__(self,poker_hand):
-		self.hand = poker_hand
-		self.high = self.hand[-1].value
-		self.low = self.hand[-3].value
-		self.string = 'Two Pair: {}s and {}s'.format(self.hand[-1].name,self.hand[-3].name)
+	def __init__(self,card_list):
+		self.cards = card_list
+		self.high = self.cards[-1].value
+		self.low = self.cards[-3].value
+		self.string = 'Two Pair: {}s and {}s'.format(self.cards[-1].name,self.cards[-3].name)
 
 	def __lt__(self, other):
 		if self.rank == other.rank:
 			if self.high == other.high:
 				if self.low == other.low:
-					return self.hand[0].value < other.hand[0].value
+					return self.cards[0].value < other.cards[0].value
 				return self.low < other.low
 			return self.high < other.high
 		return self.rank < other.rank
@@ -143,7 +140,7 @@ class TwoPair(PokerHandType):
 		if self.rank == other.rank:
 			if self.high == other.high:
 				if self.low == other.low:
-					return self.hand[0].value <= other.hand[0].value
+					return self.cards[0].value <= other.cards[0].value
 				return self.low < other.low
 			return self.high < other.high
 		return self.rank < other.rank
@@ -152,21 +149,21 @@ class TwoPair(PokerHandType):
 		if self.rank == other.rank:
 			if self.high == other.high:
 				if self.low == other.low:
-					return self.hand[0].value == other.hand[0].value
+					return self.cards[0].value == other.cards[0].value
 		return False
 
 	def __ne__(self, other):
 		if self.rank == other.rank:
 			if self.high == other.high:
 				if self.low == other.low:
-					return self.hand[0].value != other.hand[0].value
+					return self.cards[0].value != other.cards[0].value
 		return True
 
 	def __gt__(self, other):
 		if self.rank == other.rank:
 			if self.high == other.high:
 				if self.low == other.low:
-					return self.hand[0].value > other.hand[0].value
+					return self.cards[0].value > other.cards[0].value
 				return self.low > other.low
 			return self.high > other.high
 		return self.rank > other.rank
@@ -175,7 +172,7 @@ class TwoPair(PokerHandType):
 		if self.rank == other.rank:
 			if self.high == other.high:
 				if self.low == other.low:
-					return self.hand[0].value >= other.hand[0].value
+					return self.cards[0].value >= other.cards[0].value
 				return self.low > other.low
 			return self.high > other.high
 		return self.rank > other.rank
@@ -183,58 +180,58 @@ class TwoPair(PokerHandType):
 class Trips(PokerHandType):
 	rank = 3
 
-	def __init__(self,poker_hand):
-		self.hand = poker_hand
-		self.trips = self.hand[-1].value
-		self.string = 'Set of {}s'.format(self.hand[-1].name)
+	def __init__(self,card_list):
+		self.cards = card_list
+		self.trips = self.cards[-1].value
+		self.string = 'Set of {}s'.format(self.cards[-1].name)
 
 	def __lt__(self, other):
 		if self.rank == other.rank:
 			if self.trips == other.trips:
-				return self.hand < other.hand
+				return self.cards < other.cards
 			return self.trips < other.trips
 		return self.rank < other.rank
 
 	def __le__(self, other):
 		if self.rank == other.rank:
 			if self.trips == other.trips:
-				return self.hand <= other.hand
+				return self.cards <= other.cards
 			return self.trips < other.trips
 		return self.rank < other.rank
 
 	def __eq__(self, other):
 		if self.rank == other.rank:
 			if self.trips == other.trips:
-				return self.hand == other.hand
+				return self.cards == other.cards
 		return False
 
 	def __ne__(self, other):
 		if self.rank == other.rank:
 			if self.trips == other.trips:
-				return self.hand != other.hand
+				return self.cards != other.cards
 		return True
 
 	def __gt__(self, other):
 		if self.rank == other.rank:
 			if self.trips == other.trips:
-				return self.hand > other.hand
+				return self.cards > other.cards
 			return self.trips > other.trips
 		return self.rank > other.rank
 
 	def __ge__(self, other):
 		if self.rank == other.rank:
 			if self.trips == other.trips:
-				return self.hand >= other.hand
+				return self.cards >= other.cards
 			return self.trips > other.trips
 		return self.rank > other.rank
 
 class Straight(PokerHandType):
 	rank = 4
 
-	def __init__(self,poker_hand):
-		self.hand = poker_hand
-		self.to_the = self.hand[-1].value
-		self.string = 'Straight to the {}'.format(self.hand[-1].name)
+	def __init__(self,card_list):
+		self.cards = card_list
+		self.to_the = self.cards[-1].value
+		self.string = 'Straight to the {}'.format(self.cards[-1].name)
 
 	def __lt__(self, other):
 		if self.rank == other.rank:
@@ -269,49 +266,49 @@ class Straight(PokerHandType):
 class Flush(PokerHandType):
 	rank = 5
 
-	def __init__(self,poker_hand):
-		self.hand = poker_hand
-		self.to_the = self.hand[-1].value
-		self.string = 'Flush in {} to the {}'.format(self.hand[-1].suitname,self.hand[-1].name)
+	def __init__(self,card_list):
+		self.cards = card_list
+		self.to_the = self.cards[-1].value
+		self.string = 'Flush in {} to the {}'.format(self.cards[-1].suitname,self.cards[-1].name)
 
 	def __lt__(self, other):
 		if self.rank == other.rank:
-			return self.hand < other.hand
+			return self.cards < other.cards
 		return self.rank < other.rank
 
 	def __le__(self, other):
 		if self.rank == other.rank:
-			return self.hand <= other.hand
+			return self.cards <= other.cards
 		return self.rank <= other.rank
 
 	def __eq__(self, other):
 		if self.rank == other.rank:
-			return self.hand == other.hand
+			return self.cards == other.cards
 		return False
 
 	def __ne__(self, other):
 		if self.rank == other.rank:
-			return self.hand != other.hand
+			return self.cards != other.cards
 		return True
 
 	def __gt__(self, other):
 		if self.rank == other.rank:
-			return self.hand > other.hand
+			return self.cards > other.cards
 		return self.rank > other.rank
 
 	def __ge__(self, other):
 		if self.rank == other.rank:
-			return self.hand >= other.hand
+			return self.cards >= other.cards
 		return self.rank >= other.rank
 
 class FullHouse(PokerHandType):
 	rank = 6
 
-	def __init__(self,poker_hand):
-		self.hand = poker_hand
-		self.high = self.hand[-1].value
-		self.low = self.hand[0].value
-		self.string = 'Full House, {}s full of {}s'.format(self.hand[-1].name,self.hand[0].name)
+	def __init__(self,card_list):
+		self.cards = card_list
+		self.high = self.cards[-1].value
+		self.low = self.cards[0].value
+		self.string = 'Full House, {}s full of {}s'.format(self.cards[-1].name,self.cards[0].name)
 
 	def __lt__(self, other):
 		if self.rank == other.rank:
@@ -356,61 +353,61 @@ class FullHouse(PokerHandType):
 class Quads(PokerHandType):
 	rank = 7
 
-	def __init__(self,poker_hand):
-		self.hand = poker_hand
-		self.quads = self.hand[-1].value
-		self.string = 'Four {}s'.format(self.hand[-1].name)
+	def __init__(self,card_list):
+		self.cards = card_list
+		self.quads = self.cards[-1].value
+		self.string = 'Four {}s'.format(self.cards[-1].name)
 
 	def __lt__(self, other):
 		if self.rank == other.rank:
 			if self.quads == other.quads:
-				return self.hand[0].value < other.hand[0].value
+				return self.cards[0].value < other.cards[0].value
 			return self.quads < other.quads
 		return self.rank < other.rank
 
 	def __le__(self, other):
 		if self.rank == other.rank:
 			if self.quads == other.quads:
-				return self.hand[0].value <= other.hand[0].value
+				return self.cards[0].value <= other.cards[0].value
 			return self.quads < other.quads
 		return self.rank < other.rank
 
 	def __eq__(self, other):
 		if self.rank == other.rank:
 			if self.quads == other.quads:
-				return self.hand[0].value == other.hand[0].value
+				return self.cards[0].value == other.cards[0].value
 		return False
 
 	def __ne__(self, other):
 		if self.rank == other.rank:
 			if self.quads == other.quads:
-				return self.hand[0].value != other.hand[0].value
+				return self.cards[0].value != other.cards[0].value
 		return True
 
 	def __gt__(self, other):
 		if self.rank == other.rank:
 			if self.quads == other.quads:
-				return self.hand[0].value > other.hand[0].value
+				return self.cards[0].value > other.cards[0].value
 			return self.quads > other.quads
 		return self.rank > other.rank
 
 	def __ge__(self, other):
 		if self.rank == other.rank:
 			if self.quads == other.quads:
-				return self.hand[0].value >= other.hand[0].value
+				return self.cards[0].value >= other.cards[0].value
 			return self.quads > other.quads
 		return self.rank > other.rank
 
 class StraightFlush(PokerHandType):
 	rank = 8
 
-	def __init__(self,poker_hand):
-		self.hand = poker_hand
-		self.to_the = self.hand[-1].value
+	def __init__(self,card_list):
+		self.cards = card_list
+		self.to_the = self.cards[-1].value
 		if self.to_the < 12:
-			self.string = 'Straight {} flush to the {}'.format(self.hand[-1].suitname,self.hand[-1].name)
+			self.string = 'Straight {} flush to the {}'.format(self.cards[-1].suitname,self.cards[-1].name)
 		else:
-			self.string = 'Royal flush in {}'.format(self.hand[-1].suitname,self.hand[-1].name)
+			self.string = 'Royal flush in {}'.format(self.cards[-1].suitname,self.cards[-1].name)
 
 	def __lt__(self, other):
 		if self.rank == other.rank:
